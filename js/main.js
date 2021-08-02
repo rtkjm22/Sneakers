1e4e1ed9c343a79f32df7794d9583904a31ab970
ryotak1998 <74517286+rtkjm22@users.noreply.github.com>
2021-08-03 05:36:58 +0900

    wip: cssの肉付け　jsエフェクトの追加
    div.header_nav::before z-indexが上手く行っていない
    slick.js レスポンシブ対応

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
 "use strict";
+
+$(function(){
+    $('.slick_slider').slick({
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 5000,
+        cssEase: "linear",
+        slidesToShow: 4,
+        swipe: false,
+        arrows: false,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        responsive: [
+            {
+                breakpoint: 750,
+                settings: {
+                    slidesToShow: 3,
+                },
+            }
+        ],
+    });
+});
+
+//ハンバーガーメニューのエフェクト
+const ham = document.querySelector('.ham');
+const header_nav = document.querySelector('.header_nav');
+
+ham.addEventListener('click', () => {
+    if (!ham.classList.contains('active')) {
+        ham.classList.add('active');
+        header_nav.classList.add('active');
+    } else {
+        ham.classList.remove('active');
+        header_nav.classList.remove('active');
+    }
+});
