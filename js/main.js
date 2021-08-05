2e5ca7f5fa62af2713386d78a14a3ab37d867fca
ryotak1998 <74517286+rtkjm22@users.noreply.github.com>
2021-08-05 15:52:47 +0900

    レスポンシブデザインの修正　featureのエフェクト追加
    レスポンシブデザイン
    1.slick
    2.contact
    3.section
    f_itemにエフェクトを追加

--- js/main.js
+++ js/main.js
@@ -13,11 +13,23 @@ $(function(){
         pauseOnHover: false,
         responsive: [
             {
-                breakpoint: 750,
+                breakpoint: 1200,
                 settings: {
                     slidesToShow: 3,
                 },
-            }
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
         ],
     });
 });
@@ -25,13 +37,50 @@ $(function(){
 //ハンバーガーメニューのエフェクト
 const ham = document.querySelector('.ham');
 const header_nav = document.querySelector('.header_nav');
+const overlay = document.querySelector('.overlay');
+const inside_link = document.querySelectorAll('.inside_link');
 
 ham.addEventListener('click', () => {
     if (!ham.classList.contains('active')) {
         ham.classList.add('active');
         header_nav.classList.add('active');
+        overlay.classList.add('active');
     } else {
         ham.classList.remove('active');
         header_nav.classList.remove('active');
+        overlay.classList.remove('active');
+    }
+
+    for (let i=0;i<inside_link.length;i++) {
+        inside_link[i].addEventListener('click', () => {
+            ham.classList.remove('active');
+            header_nav.classList.remove('active');
+            overlay.classList.remove('active');
+        });
+    }
+});
+
+overlay.addEventListener('click', () => {
+    if (ham.classList.contains('active')) {
+        ham.classList.remove('active');
+        header_nav.classList.remove('active');
+        overlay.classList.remove('active');
+    }
+});
+
+//feature 画像のエフェクト追加
+
+const f_item = document.querySelectorAll('.f_item');
+const f_item_img = document.querySelectorAll('.f_item img');
+// console.log(f_item_img[5]);
+
+window.addEventListener('scroll', () => {   
+    for (let i=0;i<f_item.length;i++) {
+        const rect = f_item_img[i].getBoundingClientRect().top;
+        const gap  = f_item_img[i].clientHeight * .3;
+
+        if (window.innerHeight > rect + gap) {
+            f_item_img[i].classList.add('active');
+        }
     }
 });
